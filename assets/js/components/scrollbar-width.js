5cdc4e7f075f4e3e38870303c19ed3981c40d868
Andrii Marushchak <31574759+andrii-marushchak@users.noreply.github.com>
2022-06-15 17:44:10 +0300

    Update scrollbar-width.js

--- assets/js/components/scrollbar-width.js
+++ assets/js/components/scrollbar-width.js
@@ -1,7 +1,7 @@
 import getScrollbarWidth from "get-scrollbar-width"
 
 const setScrollbarWidthCSSVariable = () => {
-    if (document.getElementsByTagName('body').offsetHeight > document.documentElement.clientHeight) {
+    if (document.querySelector('body').offsetHeight > document.documentElement.clientHeight) {
         document.documentElement.style.setProperty('--scroll-width', getScrollbarWidth() + 'px')
     } else {
         document.documentElement.style.setProperty('--scroll-width', 0 + 'px')
